546df398e33b50b757800a0541ce53496dac9cf4
Taekkyun Kim <taekkyun.kim@lge.com>
2022-02-08 14:21:52 +0900

    scripts modified

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Installed from web browser
 # chrome
 function chrome() {
 	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -9,59 +10,57 @@ function chrome() {
 # citrix
 # sudo ln -s /usr/share/ca-certificates/mozilla/* /opt/Citrix/ICAClient/keystore/cacerts # if SSL error 4
 
-# snap install
-function snap() {
-	sudo snap install --classic \
-		vlc \
-		code
-	}
-
-# apt install
+# Installed from apt
 function apt() {
-	sudo apt update && sudo apt upgrade -y && sudo apt install -y \
-		vim \
-		net-tools \
-		openssh-server \
-		python3 \
-		python-is-python3 \
-		python3-pip \
-		fcitx-hangul \ # https://velog.io/@ddubbu/%EC%9A%B0%EB%B6%84%ED%88%AC-20.04-%ED%95%9C%EA%B8%80-%EC%9E%85%EB%A0%A5-%EB%B0%A9%EB%B2%95-feat.-fcitx
-		git \
-		tig \
-		htop \
-		tree \
-		cifs-utils \
-		nfs-common
-	}
+  sudo apt update && sudo apt upgrade -y && sudo apt install -y \
+  vim \
+  net-tools \
+  openssh-server \
+  python3 \
+  python-is-python3 \
+  python3-pip \
+  fcitx-hangul \
+  git \
+  tig \
+  htop \
+  tree \
+  cifs-utils \
+  nfs-common \
+  cowsay \
+  xcowsay \
+  nfs-kernel-server \
+  gcc-aarch64-linux-gnu \
+  g++-aarch64-linux-gnu
+}
 
 # install docker
 function docker() {
-	sudo apt install -y \
-		apt-transport-https \
-		ca-certificates \
-		curl \
-		gnupg \
-		lsb-release
-			curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-			echo \
-				"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-				$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-							sudo apt-get update
-							sudo apt-get install -y docker-ce docker-ce-cli containered.io 
-							sudo usermod -aG docker $USER
-						}
+  sudo apt install -y \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  echo \
+    "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+  sudo apt-get install -y docker-ce docker-ce-cli containered.io 
+  sudo usermod -aG docker $USER
+}
 
 # install gitlab runner
 function gitlab() {
-	curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | sudo bash
-	sudo apt-get install gitlab-runner
+  curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | sudo bash
+  sudo apt-get install gitlab-runner
 }
 
+gitlab
 
 function pip() {
-	pip install \
-		streamlit \
-		--upgrade protobuf
-	}
-apt
-snap
+  pip install \
+    streamlit \
+    --upgrade protobuf
+}
+# pip()
--- scripts/mount.sh
+++ scripts/mount.sh
@@ -5,11 +5,13 @@ if [ $# -ne 1 ];then
 fi
 
 USER=$1
-# MOUNT_DIR='/mnt/motional_database'
-# NAS_DIR='//10.158.10.28/Motional_Database'
+
+MOUNT_DIR='/mnt/motional_database'
+NAS_DIR='//10.158.10.28/Motional_Database'
+sudo mkdir -p ${MOUNT_DIR}
+sudo mount -v -t cifs -o username="${USER}",vers=2.0,domain=LGE,uid=$(id -u),gid=$(id -g),forceuid,forcegid,sec=ntlmsspi ${NAS_DIR} ${MOUNT_DIR}
+
 MOUNT_DIR='/mnt/oms_database'
 NAS_DIR='//10.158.10.24/OMS_Database'
-
-
 sudo mkdir -p ${MOUNT_DIR}
 sudo mount -v -t cifs -o username="${USER}",vers=2.0,domain=LGE,uid=$(id -u),gid=$(id -g),forceuid,forcegid,sec=ntlmsspi ${NAS_DIR} ${MOUNT_DIR}
